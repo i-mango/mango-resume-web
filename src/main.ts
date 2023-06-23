5ba0131d6046e16d1efb6bd02dc1b28afe547d33
潘纪婷 <mango@imufeng.cn>
2023-06-23 11:02:42 +0800

    增加animate依赖并全局引用

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,8 @@ import ElementPlus from "element-plus";
 import * as Icons from "@element-plus/icons-vue";
 // element css
 import "element-plus/dist/index.css";
-
+// 动画 animate.css
+import animate from "animate.css";
 
 const app = createApp(App)
 // 注册element Icons组件
@@ -19,7 +20,7 @@ Object.keys(Icons).forEach(key => {
     app.component(key, Icons[key as keyof typeof Icons]);
 });
 app.use(ElementPlus)
-
+app.use(animate)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
