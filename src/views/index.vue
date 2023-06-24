0d44d3cd941addfeb31eee15fffe9b76d4accd8d
潘纪婷 <mango@imufeng.cn>
2023-06-24 15:02:54 +0800

    规划页面布局

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,7 +3,21 @@
 </script>
 
 <template>
+<el-container>
+  <el-main>
+    <div>
+      <!-- 左侧-->
+      <div></div>
+      <!-- 中间-->
+      <div></div>
+      <!-- 右侧-->
+      <div></div>
+    </div>
+  </el-main>
+  <el-footer>
 
+  </el-footer>
+</el-container>
 </template>
 
 <style scoped>
