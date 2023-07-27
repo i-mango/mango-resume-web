336bdce9cd49f97387875cc9cd6042dcb20b1fcd
潘纪婷 <mango@imufeng.cn>
2023-07-27 17:26:29 +0800

    基本完成个人简介页面

--- src/components/profile/profile.vue
+++ src/components/profile/profile.vue
@@ -14,7 +14,42 @@ export default {
 <template>
   <div class="profile-page">
     <el-divider content-position="center"><h1>个人简介</h1></el-divider>
-
+    <el-card style="width: 50%;margin: auto">
+      <el-descriptions
+          :column="2"
+          :border="false"
+          :size="'mini'"
+          style="width: 100%;margin: auto"
+      >
+        <el-descriptions-item align="center" span="2"><el-avatar src="src/assets/cute.jpg" class="avatar"/></el-descriptions-item>
+        <el-descriptions-item label="姓名：">{{own.name}}</el-descriptions-item>
+        <el-descriptions-item label="年龄：">{{own.age}}</el-descriptions-item>
+        <el-descriptions-item label="性别：">{{own.sex}}</el-descriptions-item>
+        <el-descriptions-item label="电话：">{{own.phoneNumber}}</el-descriptions-item>
+        <el-descriptions-item label="QQ：">
+          <div id="box">
+            <!-- 用于显示qq图标 -->
+            <div id="qq">
+              <!-- 用于显示，隐藏二维码图片 -->
+              <div id="qq-code"></div>
+            </div>
+          </div>
+        </el-descriptions-item>
+        <el-descriptions-item label="微信：">
+          <div id="box">
+            <!-- 用于显示qq图标 -->
+            <div id="wechat">
+              <!-- 用于显示，隐藏二维码图片 -->
+              <div id="code"></div>
+            </div>
+          </div>
+        </el-descriptions-item>
+        <el-descriptions-item label="Github："><a :href=own.github target="_blank"><img src="src/assets/github.svg" alt="" class="icon-size"></a></el-descriptions-item>
+        <el-descriptions-item label="博客："><a :href=own.blog target="_blank"><img src="src/assets/blog.ico" alt="" class="icon-size"></a></el-descriptions-item>
+        <el-descriptions-item label="Gitee："><a :href=own.gitee target="_blank"><img src="src/assets/gitee.png" alt="" class="icon-size"></a></el-descriptions-item>
+        <el-descriptions-item label="总网站："><a :href=own.resume target="_blank"><img src="src/assets/favicon.ico" alt="" class="icon-size"></a></el-descriptions-item>
+      </el-descriptions>
+    </el-card>
   </div>
 </template>
 
