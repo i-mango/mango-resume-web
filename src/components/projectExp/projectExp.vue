21a918c800dc78e0f20ac86ed9bcf4257d4373b0
潘纪婷 <admin@manggo.cn>
2023-08-01 09:27:36 +0800

    完成项目经验页面

--- src/components/projectExp/projectExp.vue
+++ src/components/projectExp/projectExp.vue
@@ -4,7 +4,7 @@ export default {
   name: 'projectExp',
   data() {
     return {
-      projectExp: ownInfo.projectExperience,
+      projectExpList: ownInfo.projectExperience,
     };
   }
 }
@@ -14,15 +14,13 @@ export default {
   <div>
     <el-divider content-position="center"><h1>项目经验</h1></el-divider>
     <el-row>
-      <el-col>
+      <el-col v-for="(project,index) in projectExpList" :key="index" :span="6">
         <el-card class="box-card">
-          <div class="text item">
-            <h3>项目名称：{{ projectExp.projectName }}</h3>
-            <p>项目描述：{{ projectExp.projectDescription }}</p>
-            <p>项目职责：{{ projectExp.projectDuty }}</p>
-            <p>项目技术：{{ projectExp.projectSkill }}</p>
-            <p>项目时间：{{ projectExp.projectTime }}</p>
-          </div>
+          <h3>项目名称：{{ project.name }}</h3>
+          <p>项目时间：{{ project.time }}</p>
+          <p>项目职责：{{ project.duty }}</p>
+          <p>项目技术：{{ project.skill }}</p>
+          <p>项目描述：{{ project.detail }}</p>
         </el-card>
       </el-col>
     </el-row>
@@ -30,5 +28,7 @@ export default {
 </template>
 
 <style scoped>
-
+.box-card{
+  width: 80%;
+}
 </style>
