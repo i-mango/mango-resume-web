2ffc19744ce21d0f65dc2ad4d90f24c37d53232e
潘纪婷 <admin@manggo.cn>
2023-08-01 08:58:04 +0800

    设计页面的基础构成

--- src/components/projectExp/projectExp.vue
+++ src/components/projectExp/projectExp.vue
@@ -3,7 +3,22 @@
 </script>
 
 <template>
-
+  <div>
+    <el-divider content-position="center"><h1>项目经验</h1></el-divider>
+    <el-row>
+      <el-col>
+        <el-card class="box-card">
+          <div class="text item">
+            <h3>项目名称：{{ projectExp.projectName }}</h3>
+            <p>项目描述：{{ projectExp.projectDescription }}</p>
+            <p>项目职责：{{ projectExp.projectDuty }}</p>
+            <p>项目技术：{{ projectExp.projectSkill }}</p>
+            <p>项目时间：{{ projectExp.projectTime }}</p>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <style scoped>
